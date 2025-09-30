5e2e826acd0daa51882ce5d6902e9d1a0e27e259
Drashti-Mehta-13 <drashtimehta08@gmail.com>
2025-09-30 10:27:52 +0530

    Add files via upload

---
+++ package.py
@@ -0,0 +1,16 @@
+# import earth.asian_countries
+# import earth.european_countrie
+# import earth.north_american_countries
+
+from earth import asian_countries as ac
+from earth import european_countrie as ec
+from earth import north_american_countries as na
+
+print(ac.asian_countries[4])
+#Bangladesh
+
+print(ec.european_countries[1:5])
+#['Andorra', 'Armenia', 'Austria', 'Azerbaijan']
+
+print(na.north_american_countries[10])
+#Grenada
