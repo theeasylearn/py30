13e8e45f333bbac746edb60d72da366c423f4440
Drashti-Mehta-13 <drashtimehta08@gmail.com>
2025-09-30 10:28:48 +0530

    Add files via upload

---
+++ earth/european_countrie.py
@@ -0,0 +1,1 @@
+european_countries = ["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan", "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Georgia", "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Kazakhstan", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta"]
