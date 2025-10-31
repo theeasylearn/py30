ae8faa1b48629fdc0a71ba175bd0e3fca7a85967
theeasylearn <theeasylearn@gmail.com>
2025-10-31 11:52:37 +0530

    oop

---
+++ oop/68_oop_1.py
@@ -0,0 +1,21 @@
+#create class 
+class Human:
+    #create function
+    def walk(self):
+        print("I can walk...")
+    def talk(self):
+        print("I can talk...")
+    def eat(self):
+        print("I can eat...")
+
+#create object 
+ankit = Human()
+ankit.walk()
+ankit.talk()
+ankit.eat()
+
+#create another object
+mohit = Human()
+mohit.eat()
+mohit.walk()
+mohit.talk()
